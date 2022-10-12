01c4c622050f81b7c0c6f2b23e592db23909dd12
Karjhan <alex_mar1999@yahoo.com>
2022-10-12 19:38:57 +0300

    changed the css a bit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,13 @@
 //import all data
 //import {data} from '../pizzas.js';
 Promise.all([fetch(`../api/pizzas`),fetch(`../api/allergens`)]).then(responses=>Promise.all(responses.map(response=>response.json()))).then(objects=>{
-  //list of all pizzas from pizzas.js
+  //variable to check if addButton was pressed
+  var pressed = false;
+
+  //list of all pizzas from pizzas.json
   const allPizzasList = objects[0];
 
-  //list of all allergens from pizzas.js
+  //list of all allergens from pizzas.json
   const allAllergensList = objects[1];
 
   //function to add pizzas to order
@@ -53,7 +56,7 @@ Promise.all([fetch(`../api/pizzas`),fetch(`../api/allergens`)]).then(responses=>
         <button class="addButton">
         <div class="svg-wrapper-1">
           <div class="svg-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" width="24" height="24">
               <path fill="none" d="M0 0h24v24H0z"></path>
               <path fill="currentColor" d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
             </svg>
@@ -106,6 +109,7 @@ Promise.all([fetch(`../api/pizzas`),fetch(`../api/allergens`)]).then(responses=>
         let price = allPizzasList.filter(elem => elem.name === pizzaName)[0].price;
         addToBasket(pizzaId,amount,price,pizzaName);
         numberNode.textContent = "1";
+        pressed = true;
       });
     });
   }
@@ -124,8 +128,7 @@ Promise.all([fetch(`../api/pizzas`),fetch(`../api/allergens`)]).then(responses=>
   //<video class="bgVideo" autoplay muted loop><source src="https://youtu.be/2ss1BHH-leI" type="video/mp4" /></video>
   //function for putting togheter allergen components
   function allAllergenComponent(allergenList){
-    return `<div class="allAllergensContainer">${allergenList.map(allergen => allergenHTMLcomponent(allergen)).join("")}
-    </div>`;
+    return `<div class="allAllergensContainer">${allergenList.map(allergen => allergenHTMLcomponent(allergen)).join("")}</div>`;
   }
 
   //function create HTML for pizzas
@@ -137,7 +140,7 @@ Promise.all([fetch(`../api/pizzas`),fetch(`../api/allergens`)]).then(responses=>
     document.querySelectorAll(".allergenCheck").forEach((node)=>{
       node.addEventListener("click",(event)=>{
         let idOfAllergensChecked = Array.from(document.querySelectorAll(".allergenCheck")).filter(node => node.checked === true).map(node => parseInt(node.name.split("allergen")[1]));
-        let filteredPizzas = allPizzasList.filter(pizza => idOfAllergensChecked.every(elem => pizza.allergens.includes(elem)));
+        let filteredPizzas = allPizzasList.filter(pizza => idOfAllergensChecked.every(elem => !pizza.allergens.includes(elem)));
         clearPizzasDivs();
         createPizzasHTML(filteredPizzas);
       });
@@ -149,7 +152,6 @@ Promise.all([fetch(`../api/pizzas`),fetch(`../api/allergens`)]).then(responses=>
     return `<div class="navBarContainer"><nav><h1>Cold <i class="fa-solid fa-pizza-slice"></i> Pizzas</h1><div class="orderContainer"><a href="../basket"><i class="fa-sharp fa-solid fa-basket-shopping"></i></a></div></nav></div>`
   }
 
-  
   function loadEvent(){
     // the HTML elements with ID are available as global variables with the ID (eg. root) but it is better if you 
     const rootElement = document.getElementById("root");
