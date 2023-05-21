d4535a3e5e1fd8e2c6dd3bdb56415d16429a0153
claudiumt <claudium.tudor@gmail.com>
2023-05-21 13:28:27 +0300

    Reformat code

--- public/javascripts/tools.js
+++ public/javascripts/tools.js
@@ -2,17 +2,17 @@ const readFile = require("./fileReader.js");
 const writeFile = require("./fileWriter.js");
 
 function assignID(obj) {
-  let usedIDs = [];
-  obj.orders.forEach((order) => usedIDs.push(order.id));
-  const ID = usedIDs
-    .map((_, i, arr) => (!arr.slice(0, i).includes(i + 1) ? i + 1 : null))
-    .find((id) => !usedIDs.includes(id));
+    let usedIDs = [];
+    obj.orders.forEach((order) => usedIDs.push(order.id));
+    const ID = usedIDs
+        .map((_, i, arr) => (!arr.slice(0, i).includes(i + 1) ? i + 1 : null))
+        .find((id) => !usedIDs.includes(id));
 
-  return ID;
+    return ID;
 }
 
 module.exports = {
-  readFile,
-  writeFile, 
-  assignID,
+    readFile,
+    writeFile,
+    assignID,
 };
